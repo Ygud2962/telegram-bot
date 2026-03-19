3a9094d61110f45b40990b1952068ced834a585e
Ygud2962 <92030953+Ygud2962@users.noreply.github.com>
2026-03-19 10:19:12 +0300

    prawki

--- deploy.bat
+++ deploy.bat
@@ -1,12 +1,59 @@
 @echo off
-git checkout dev
-git add .
-set /p msg="Edit summary: "
-git commit -m "%msg%"
-git push origin dev
-git checkout main
-git merge dev
-git push origin main
+chcp 65001 > nul
 echo.
-echo Done! The bot has been updated.
+echo ========================================
+echo   DEPLOY - СШ №3 Шифровальщик
+echo ========================================
+echo.
+
+set /p MSG="Commit message (English): "
+if "%MSG%"=="" set MSG=update
+
+cd /d "%~dp0"
+
+echo.
+echo [1/5] Switching to dev...
+git checkout dev
+if errorlevel 1 goto error
+
+echo [2/5] Committing changes...
+git add .
+git commit -m "%MSG%"
+
+echo [3/5] Pushing dev...
+git push origin dev
+if errorlevel 1 goto error
+
+echo [4/5] Switching to main...
+git checkout main
+
+echo [4/5] Syncing main with remote...
+git fetch origin main
+git reset --hard origin/main
+
+echo [5/5] Merging dev into main...
+git merge dev --no-edit
+if errorlevel 1 goto error
+
+echo [5/5] Pushing main...
+git push origin main
+if errorlevel 1 goto error
+
+git checkout dev
+
+echo.
+echo ========================================
+echo   Done! The bot has been updated.
+echo ========================================
+echo.
 pause
+exit /b 0
+
+:error
+echo.
+echo ========================================
+echo   ERROR! Check the output above.
+echo ========================================
+echo.
+pause
+exit /b 1
